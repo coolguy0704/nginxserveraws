85ad6cc33439ddf25e14d763696b489d7c44341f
Prakash Rao <mb16@iitbbs.ac.in>
2022-10-31 20:10:31 +0900

    remove private subnet resource

--- vpc.tf
+++ vpc.tf
@@ -18,12 +18,3 @@ resource "aws_subnet" "nginx-public-subnet-1" {
     }
   
 }
-
-resource "aws_subnet" "nginx-private-subnet-1" {
-    vpc_id = "${aws_vpc.nginx-vpc.id}"
-    cidr_block = "${var.VPC_PRIVATE_SUBNET_CIDR}"
-    availability_zone = "${var.AWS_AZ}"
-    tags = {
-      "Name" = "nginx-private-subnet-1"
-    }
-}
