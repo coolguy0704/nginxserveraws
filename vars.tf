761e40c393f6a0d17074d7e51cee886819bdeee4
Prakash Rao <mb16@iitbbs.ac.in>
2022-10-31 20:08:36 +0900

    remove private subnet variable

--- vars.tf
+++ vars.tf
@@ -17,11 +17,6 @@ variable "VPC_PUBLIC_SUBNET_CIDR" {
     default = "10.0.1.0/24"
 }
 
-variable "VPC_PRIVATE_SUBNET_CIDR" {
-    type = string
-    default = "10.0.2.0/24"
-}
-
 variable "sg_ports" {
     type = list(number)
     default = [ 22,80 ]
